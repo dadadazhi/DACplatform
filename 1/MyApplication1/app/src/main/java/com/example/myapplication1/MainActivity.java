b32b9ae713946576793ab0f11176b7affb3c2dae
dadadazhi <34294915+dadadazhi@users.noreply.github.com>
2017-12-27 21:58:37 +0800

    1

--- 1/MyApplication1/app/src/main/java/com/example/myapplication1/MainActivity.java
+++ 1/MyApplication1/app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.myapplication1;
 
+import android.app.AlertDialog;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
@@ -66,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
                 s="";
                 break;
             case 3:
-                this.finish();
+                onDialogClick1();
                 break;
 
         }
@@ -661,14 +663,29 @@ public class MainActivity extends AppCompatActivity {
         PendingIntent pIntent = PendingIntent.getActivity(this,1,intent,0);
         builder.setContentIntent(pIntent);
         //设置震动
-        //long[] vibrate = {100,200,100,200};
-        //builder.setVibrate(vibrate);
+        long[] vibrate = {100,200,100,200};
+        builder.setVibrate(vibrate);
         builder.setDefaults(NotificationCompat.DEFAULT_ALL);
         Notification notification = builder.build();
         manger.notify(TYPE_Normal,notification);
 
     }
+    public void onDialogClick1( ){
+        new AlertDialog.Builder(MainActivity.this)
+                .setIcon(android.R.drawable.alert_dark_frame)
+                .setTitle("注意!!!")
+                .setMessage("确定要退出么???")
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        finish();//Exit Activity
+                    }
+                })
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                    }
+                }).create().show();
 
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
