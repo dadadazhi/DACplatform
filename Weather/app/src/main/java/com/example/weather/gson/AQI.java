3b984c7b4ce3c6fac50447e309467d565703ddbb
dadadazhi <34294915+dadadazhi@users.noreply.github.com>
2018-02-19 17:57:02 +0800

    天气界面
    Signed-off-by: dadadazhi <34294915+dadadazhi@users.noreply.github.com>

---
+++ Weather/app/src/main/java/com/example/weather/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.example.weather.gson;
+
+/**
+ * Created by 智治 on 2018/2/19.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
