9488442f425aa1994606ae227ba1a0d8b5deed54
dadadazhi <34294915+dadadazhi@users.noreply.github.com>
2018-02-27 20:13:16 +0800

    android stdio 崩了，重装

--- Weather2/app/src/main/java/com/example/weather2/WeatherActivity.java
+++ Weather2/app/src/main/java/com/example/weather2/WeatherActivity.java
@@ -17,6 +17,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
+import com.bumptech.glide.Glide;
 import com.example.weather2.gson.Forecast;
 import com.example.weather2.gson.Suggestion;
 import com.example.weather2.gson.Weather;
@@ -68,6 +69,12 @@ public class WeatherActivity extends AppCompatActivity {
         swipeRefresh.setColorSchemeResources(R.color.colorPrimary);
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         String weatherString = prefs.getString("weather", null);
+        String bingPic=prefs.getString("bing_pic",null);
+        if(bingPic!=null){
+            Glide.with(this).load(bingPic).into(bingPicImg);
+        }else{
+            loadBingPic();
+        }
         if (weatherString != null) {
             //有缓存时直接解析天气数据
             Weather weather=Utility.handleWeatherResponse(weatherString);
@@ -165,6 +172,30 @@ public class WeatherActivity extends AppCompatActivity {
                         swipeRefresh.setRefreshing(false);
                     }
                 });
+            }
+        });
+        loadBingPic();
+    }
+    private void loadBingPic(){
+        String requesBingPic="http://guolin.tech/api/bing_pic";
+        HttpUtil.sendOkHtpRequest(requesBingPic, new Callback() {
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String bingPic=response.body().string();
+                SharedPreferences.Editor editor=PreferenceManager.getDefaultSharedPreferences(WeatherActivity.this).edit();
+                editor.putString("bing_pic",bingPic);
+                editor.apply();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Glide.with(WeatherActivity.this).load(bingPic).into(bingPicImg);
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
             }
         });
     }
